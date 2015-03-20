78ed35bb8e7fae0879eb480c35b6cc0de3487f95
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-03-19 21:31:50 -0400

    Thorough testing of mongos, SlaveOkat, and $readPreference.

--- tests/test_mongos_command_read_mode.py
+++ tests/test_mongos_command_read_mode.py
@@ -12,10 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mockupdb import MockupDB, going
+import itertools
+
+from mockupdb import MockupDB, going, QUERY_FLAGS
 from pymongo import MongoClient, ReadPreference
+from pymongo.read_preferences import (make_read_preference,
+                                      read_pref_mode_from_name,
+                                      _MONGOS_MODES)
 
 from tests import unittest
+from tests.operations import operations
 
 
 class TestMongosCommandReadMode(unittest.TestCase):
@@ -39,6 +45,63 @@ class TestMongosCommandReadMode(unittest.TestCase):
             command.ok(result=[{}])
             self.assertEqual({'mode': 'secondary'},
                              command.doc.get('$readPreference'))
+
+
+def create_mongos_read_mode_test(mode, operation):
+    def test(self):
+        server = MockupDB()
+        self.addCleanup(server.stop)
+        server.run()
+        server.autoresponds('ismaster', ismaster=True, msg='isdbgrid',
+                            maxWireVersion=operation.wire_version)
+
+        if operation.op_type in ('always-use-secondary', 'may-use-secondary'):
+            slave_ok = mode != 'primary'
+        elif operation.op_type == 'must-use-primary':
+            slave_ok = False
+        else:
+            assert False, 'unrecognized op_type %r' % operation.op_type
+
+        pref = make_read_preference(read_pref_mode_from_name(mode),
+                                    tag_sets=None)
+
+        client = MongoClient(server.uri, read_preference=pref)
+        with going(operation.function, client):
+            request = server.receive()
+            request.reply(operation.reply)
+
+        if slave_ok:
+            self.assertTrue(request.flags & QUERY_FLAGS['SlaveOkay'],
+                            'SlaveOkay not set')
+        else:
+            self.assertFalse(request.flags & QUERY_FLAGS['SlaveOkay'],
+                             'SlaveOkay set')
+
+        if mode in ('primary', 'secondary'):
+            self.assertNotIn('$readPreference', request)
+        else:
+            self.assertEqual(pref.document, request.doc.get('$readPreference'))
+
+    return test
+
+
+def generate_mongos_read_mode_tests():
+    matrix = itertools.product(_MONGOS_MODES, operations)
+
+    for entry in matrix:
+        mode, operation = entry
+        if mode == 'primary' and operation.op_type == 'always-use-secondary':
+            # Skip something like command('foo', read_preference=SECONDARY).
+            continue
+        test = create_mongos_read_mode_test(mode, operation)
+        test_name = 'test_%s_with_mode_%s' % (
+            operation.name.replace(' ', '_'), mode)
+        test.__name__ = test_name
+        setattr(TestMongosCommandReadMode, test_name, test)
+
+
+generate_mongos_read_mode_tests()
+
 
 if __name__ == '__main__':
     unittest.main()
