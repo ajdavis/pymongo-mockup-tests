8a5d1fa2784257059f0348e726971540ac875245
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-03-20 23:29:27 -0400

    Add mapreduce, collection options, and index info ops.

--- tests/operations.py
+++ tests/operations.py
@@ -21,6 +21,12 @@ Operation = namedtuple(
     'operation',
     ['name', 'function', 'reply', 'op_type', 'wire_version', 'not_master'])
 
+not_master_reply_to_query = OpReply(
+    {'$err': 'not master'},
+    flags=REPLY_FLAGS['QueryFailure'])
+
+not_master_reply_to_command = OpReply(ok=0, errmsg='not master')
+
 operations = [
     Operation(
         'find_one',
@@ -28,29 +34,58 @@ operations = [
         reply={},
         op_type='may-use-secondary',
         wire_version=0,
-        not_master=OpReply({'$err': 'not master'},
-                           flags=REPLY_FLAGS['QueryFailure'])),
+        not_master=not_master_reply_to_query),
     Operation(
         'count',
         lambda client: client.db.collection.count(),
         reply={'n': 1},
         op_type='may-use-secondary',
         wire_version=0,
-        not_master=OpReply(ok=0, errmsg='not master')),
+        not_master=not_master_reply_to_command),
     Operation(
         'aggregate',
         lambda client: client.db.collection.aggregate([]),
         reply={'result': []},
         op_type='may-use-secondary',
         wire_version=0,
-        not_master=OpReply(ok=0, errmsg='not master')),
+        not_master=not_master_reply_to_command),
+    Operation(
+        'mapreduce',
+        lambda client: client.db.collection.map_reduce(
+            'function() {}', 'function() {}', 'out_collection'),
+        reply={'result': {'db': 'db', 'collection': 'out_collection'}},
+        op_type='must-use-primary',
+        wire_version=0,
+        not_master=not_master_reply_to_command),
+    Operation(
+        'inline_mapreduce',
+        lambda client: client.db.collection.inline_map_reduce(
+            'function() {}', 'function() {}', {'out': {'inline': 1}}),
+        reply={'results': []},
+        op_type='may-use-secondary',
+        wire_version=0,
+        not_master=not_master_reply_to_command),
+    Operation(
+        'options_old',
+        lambda client: client.db.collection.options(),
+        reply={'results': []},
+        op_type='must-use-primary',
+        wire_version=0,
+        not_master=not_master_reply_to_query),
+    Operation(
+        'options_new',
+        lambda client: client.db.collection.options(),
+        reply={'cursor': {'id': 0, 'firstBatch': []}},
+        op_type='must-use-primary',
+        wire_version=3,
+        not_master=not_master_reply_to_command),
     Operation(
         'command',
         lambda client: client.db.command('foo'),
         reply={'ok': 1},
         op_type='must-use-primary',  # Ignores client's read preference.
         wire_version=0,
-        not_master=OpReply(ok=0, errmsg='not master')),
+        not_master=not_master_reply_to_command),
     Operation(
         'secondary command',
         lambda client:
@@ -65,13 +100,26 @@ operations = [
         reply=[],
         op_type='must-use-primary',
         wire_version=0,
-        not_master=OpReply({'$err': 'not master'},
-                           flags=REPLY_FLAGS['QueryFailure'])),
+        not_master=not_master_reply_to_query),
     Operation(
         'listCollections',
         lambda client: client.db.collection_names(),
         reply={'cursor': {'id': 0, 'firstBatch': []}},
         op_type='must-use-primary',
         wire_version=3,
-        not_master=OpReply(ok=0, errmsg='not master')),
+        not_master=not_master_reply_to_command),
+    Operation(
+        'system_indexes',
+        lambda client: client.db.collection.index_information(),
+        reply=[],
+        op_type='must-use-primary',
+        wire_version=0,
+        not_master=not_master_reply_to_query),
+    Operation(
+        'listIndexes',
+        lambda client: client.db.collection.index_information(),
+        reply={'cursor': {'id': 0, 'firstBatch': []}},
+        op_type='must-use-primary',
+        wire_version=3,
+        not_master=not_master_reply_to_command),
 ]
